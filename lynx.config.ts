5d5ae00b671bb85a733e184a6049db0962b8ff49
Mykyta Nazarov <nnazarov55@gmail.com>
2025-03-14 22:07:27 +0200

    add Tailwind CSS configuration and integrate with project

--- lynx.config.ts
+++ lynx.config.ts
@@ -2,9 +2,14 @@ import { defineConfig } from '@lynx-js/rspeedy'
 
 import { pluginQRCode } from '@lynx-js/qrcode-rsbuild-plugin'
 import { pluginReactLynx } from '@lynx-js/react-rsbuild-plugin'
-
+import { pluginTailwindCSS } from 'rsbuild-plugin-tailwindcss'
 export default defineConfig({
   plugins: [
+    pluginTailwindCSS({
+      config: './tailwind.config.js',
+      include: /\.[jt]sx?/,
+      exclude: ["./src/store/**", /[\\/]node_modules[\\/]/],
+    }),
     pluginQRCode({
       schema(url) {
         // We use `?fullscreen=true` to open the page in LynxExplorer in full screen mode
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import { root } from '@lynx-js/react'
 
 import { App } from './App.js'
-
+import 'tailwindcss/utilities.css'
 root.render(<App />)
 
 if (import.meta.webpackHot) {
